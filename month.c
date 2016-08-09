35a5d0ee7662bbdec914f056c1c7076219c211f8
jjmhere <ammon.jia@gmail.com>
2016-08-09 17:39:23 +0800

    change month.c

--- month.c
+++ month.c
@@ -4,27 +4,28 @@ int main(void)
 {
 	char a[][11] ={"January","February","March","April","May","June",
 			"July","August","September","October","November","December"};
-	char *b[] ={"0","January","February","March","April","May","June",
+	char *b[] ={"January","February","March","April","May","June",
 			"July","August","September","October","November","December"};
-	int i = 1;
+	int i;
 	
 	printf("请输入月份：");
 	scanf("%d",&i);
 	i--;
-	while(i < 12)
+	while(i < 12 && i >=0)
 	{
-		printf("您要的是%s\n",a[i]);
-		printf("\n a[%d]的地址是:%p\n",i,&a[i]);
+		printf("您要的是%s\t",a[i]);
+		printf("a[%d]的地址是:%p\t",i,&a[i]);
 		printf("len = %d\n",strlen(a[i]));
-		printf("您要的是%s\n",b[i]);
-		printf("\n b[%d]的地址是:%p\n",i,b[i]);
-		printf("len = %d",strlen(b[i]));
+		printf("您要的是%s\t",b[i]);
+		printf("b[%d]的地址是:%p\t",i,b[i]);
+		printf("len = %d\n",strlen(b[i]));
 		printf("请输入月份：");
 		
 		scanf("%d",&i);
+		i--;
 		
 		
 	}
-	printf("sizeof(a[]) = %d,sizeof(b[]) = %d",sizeof(a),sizeof(b));
+	printf("sizeof(a[i]) = %d,sizeof(b[i]) = %d\n",sizeof(a),sizeof(b));
 	return 0;
  } 
